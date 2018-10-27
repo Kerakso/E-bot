873765d7d3d7a898203957f7a8a9c6cef3d3267b
Kerakso <20502639+Kerakso@users.noreply.github.com>
2018-10-27 17:22:59 +0200

    .

--- bot.js
+++ bot.js
@@ -84,10 +84,8 @@ bot.on("message", async msg => {
     	let liczbaWiezien = msg.guild.roles.get(wiezien).members;
     	if(liczbaWiezien.size < 3) {
     		msg.channel.send(`Posiadamy ${liczbaWiezien.size} więźniów w łagrach.. Coś mało ich mamy, trzeba któregoś robotnika wrobić w zdradę kraju.`).catch(console.error);
-    	} else if(liczbaWiezien.size > 4) {
-    		msg.channel.send(`Posiadamy ${liczbaWiezien.size} więźniów w łagrach.. Trza kogoś rozstrzelać.`).catch(console.error);
     	} else {
-    		msg.reply("foka się stała i ni mu");
+    		msg.channel.send(`Posiadamy ${liczbaWiezien.size} więźniów w łagrach.. Trza kogoś rozstrzelać.`).catch(console.error);
     	}
     }
 });
